13b99ee2761a797cb3126e577b58d391324e5b5f
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-03 13:30:08 +0200

    Fix state fN

--- src/reducers/receive.js
+++ src/reducers/receive.js
@@ -1,4 +1,4 @@
-export default function extension(state = { command: null, status: null }, action) {
+export default function extension(state = { fN: null, status: null }, action) {
   if (action.key === 'extension' && action.payload.fN && action.payload.status === 'sent') {
     console.info('Received action', action);
     return { ...state, fN: action.payload.fN, status: 'received' };
